248de7850885d7aa3daf260072738e85eae76952
Zhe Ming <79309030+zmchang3009@users.noreply.github.com>
2025-01-18 00:15:08 +0800

    implemented states to handle collate_bot convo

--- collate_bot.py
+++ collate_bot.py
@@ -3,18 +3,24 @@ import os
 from dotenv import load_dotenv
 from typing import Final
 
-from telegram import Update
-from telegram.ext import Application, CommandHandler, ContextTypes, MessageHandler, filters
+from telegram import Update, ForceReply
+from telegram.ext import Application, CommandHandler, ContextTypes, MessageHandler, ConversationHandler, filters
 
 load_dotenv()
 TOKEN: Final = os.environ.get('ZHEMS_BOT_API')
 BOT_USERNAME: Final = '@zhemsbot'
 
 
-## Responses
+## States
+TOPIC, ITEMS = range(2)
+
+
+## Global variables
 collated_responses: dict = {}
 list_owner: str = ''
 list_topic: str = ''
+message_ids: dict = {} ## Tracks notable messages
+
 
 ## Command handlers
 async def start_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
@@ -24,7 +30,15 @@ async def start_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> N
     list_topic = ''
     user: str = update.message.from_user.username
     list_owner = user
-    await update.message.reply_text(f'{user} has started a new list. Please suggest a topic or title.')
+    
+    sent_message = await update.message.reply_text(
+        f'{user} has started a new list. Please suggest a topic or title.',
+        reply_markup=ForceReply(selective=True)
+    )
+
+    message_ids[TOPIC] = sent_message.message_id
+
+    return TOPIC
 
 
 async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
@@ -38,7 +52,65 @@ async def collate_command(update: Update, context: ContextTypes.DEFAULT_TYPE) ->
     await update.message.reply_text(response)
 
 
-## Response handlers
+## Message handlers
+## Handle setting list topic (Only listens to replies)
+async def topic_handler(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Handle setting the list topic."""
+    global collated_responses, list_topic, list_owner
+    text: str = update.message.text
+    user: str = update.message.from_user.username
+    replied_id: int = update.message.reply_to_message.message_id
+    
+    if replied_id == message_ids[TOPIC] and user == list_owner:
+        list_topic = text
+        await update.message.reply_text(f'Please start suggesting items for list {list_topic}.')
+        
+        return ITEMS
+
+
+## Handle incoming list items
+async def items_handler(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Handle incoming list items."""
+    global collated_responses, list_topic, list_owner
+    message_type: str = update.message.chat.type
+    text: str = update.message.text
+    user: str = update.message.from_user.username
+
+    ## Add item to list
+    if message_type == 'group' or message_type == 'supergroup':
+        if BOT_USERNAME in text:
+            new_text: str = text.replace(BOT_USERNAME, '').strip()
+            collated_responses[user] = new_text
+            print(f'User {user} response recorded')
+        else:
+            return
+    else:
+        collated_responses[user] = text
+        print(f'User {user} response recorded')
+
+
+## Fallback handler
+async def stop_handler(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Close the list."""
+    global collated_responses, list_topic, list_owner
+
+    response: str = collate_responses()
+    await update.message.reply_text(f'No longer accepting responses for {list_topic}. Final list:\n\n{response}')
+
+    collated_responses.clear()
+    list_topic = ''
+    list_owner = ''
+
+    return ConversationHandler.END
+    
+
+## Error handlers
+async def error_handler(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Log errors caused by updates."""
+    print(f'Update {update} caused error {context.error}')
+
+
+## Helper functions
 def collate_responses() -> str:
     """Organize the user responses."""
     global collated_responses, list_topic, list_owner
@@ -50,47 +122,11 @@ def collate_responses() -> str:
     if not collated_responses:
         return 'No responses yet.'
 
-    output += f'List of {list_topic} started by {list_owner}\n'
+    output += f'{list_topic}  (by {list_owner})\n'
     for user, response in collated_responses.items():
         output += f'{user}: {response}\n'
 
     return output
-
-
-## Message handlers
-async def message_handler(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    """Handle incoming messages."""
-    global collated_responses, list_topic, list_owner
-    message_type: str = update.message.chat.type
-    text: str = update.message.text
-    user: str = update.message.from_user.username
-
-    print(f'User {user} in {message_type}: "{text}"')
-
-    ## Handle setting list topic
-    if list_topic == '' and list_owner == user and BOT_USERNAME in text:
-        list_topic = text
-        await update.message.reply_text(f'Please start suggesting items for list {list_topic}.')
-        return
-    
-    ## Add item to list
-    if message_type == 'group' or message_type == 'supergroup':
-        if BOT_USERNAME in text:
-            new_text: str = text.replace(BOT_USERNAME, '').strip()
-            collated_responses[user] = new_text
-        else:
-            return
-    else:
-        collated_responses[user] = text
-
-    print(f'User {user} response recorded')
-
-
-## Error handlers
-async def error_handler(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    """Log errors caused by updates."""
-    print(f'Update {update} caused error {context.error}')
-
 
 
 def main() -> None:
@@ -99,18 +135,26 @@ def main() -> None:
     print('Starting bot...')
     application = Application.builder().token(TOKEN).build()
 
+    ## Conversation handler
+    conv_handler = ConversationHandler(
+        entry_points=[CommandHandler('start', start_command)],
+        states={
+            TOPIC: [MessageHandler(filters.REPLY, topic_handler)],
+            ITEMS: [MessageHandler(filters.TEXT & ~filters.COMMAND, items_handler)]
+        },
+        fallbacks=[CommandHandler('stop', stop_handler)]
+    )
+    application.add_handler(conv_handler)
+
     ## Commands
     application.add_handler(CommandHandler("start", start_command))
     application.add_handler(CommandHandler("help", help_command))
     application.add_handler(CommandHandler("collate", collate_command))
-
-    ## Messages
-    application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, message_handler))
     
     ## Errors
     application.add_error_handler(error_handler)
 
-    # Run the bot until the user presses Ctrl-C
+    ## Run the bot until the user presses Ctrl-C
     print('Polling...')
     application.run_polling(allowed_updates=Update.ALL_TYPES)
 
